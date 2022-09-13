fb3c3d70dd399fa12a88a74a395370e91b448ff7
top-angel <ddwebdev2019@gmail.com>
2022-09-13 14:15:34 -0400

    constants and utils

---
+++ src/constants/defaultImages.ts
@@ -0,0 +1,1 @@
+export const DEFAULT_IMAGE = 'provider.png';
---
+++ src/utils/formatDateTime.utils.ts
@@ -0,0 +1,12 @@
+
+export const formatAMPM = (time: string) => {
+    let date = new Date(time);
+    let hours = date.getHours();
+    let minutes: any = date.getMinutes();
+    let ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+}
