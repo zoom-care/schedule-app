42d4ee34f166917f730d3a4f2866c7aff9504741
Jeremy Lewis <Jlewis@zoomcare.com>
2022-12-29 09:38:03 -0800

    routing + config

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import { Routes, Route, useNavigate } from "react-router-dom";
 import './App.css';
+import Login from './components/Login';
+import ScheduleApp from './components/ScheduleApp';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const navigate = useNavigate();
+
+ useEffect(() => {
+    const info = localStorage.getItem("user-info");
+    return navigate(info ? '/clinic' : '/login');
+  }, [navigate])
+
+return (
+  <Routes>
+    <Route path="/login" element={<Login />} />
+    <Route path="/clinic" element={<ScheduleApp />} />
+  </Routes>
+);}
 
 export default App;
