6fa09fda93b95f65da7e0244ad8c016e55c6b4ac
jjhernandezram <jjhernandezram@gmail.com>
2023-03-10 22:49:48 -0600

    refactor!: add types for component and services

---
+++ src/constants/api.constants.ts
@@ -0,0 +1,13 @@
+export enum apiUrl {
+  LOGIN = '/api/login',
+  APPOINTMENT = '/api/appointments',
+  CLINICS = '/api/clinics',
+};
+
+export enum RequestMethod {
+  GET = 'GET',
+  POST = 'POST',
+  PUT = 'PUT',
+  PATCH = 'PATCH',
+  DELETE = 'DELETE',
+}
---
+++ src/types/card.types.ts
@@ -0,0 +1,5 @@
+import { clinicWithAppointments } from './store.types';
+
+export type CardProps = {
+  appointments: clinicWithAppointments[];
+};
