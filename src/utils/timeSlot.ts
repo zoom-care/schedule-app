ef5cb3f30e64d78810af0b6636f8d5d05088d20a
juancruzgardey <juancruzgardey@hotmail.com>
2023-05-08 17:04:10 -0300

    refactor timeSlot

--- src/utils/timeSlot.ts
+++ src/utils/timeSlot.ts
@@ -1,13 +1,4 @@
 
 export const timeSlot = (startTime: string): string => {
-    const hours = startTime.split(' ')[1];
-    const [start, end] = hours.split('-');
-    return `${twelveHour(start)}`
-}
-
-const twelveHour = (time: string) =>  {
-    const [hour, minutes] = time.split(':');
-    const hourNumber: number = parseInt(hour);
-    const amOrpm = hourNumber >= 12 ? 'PM':'AM';
-    return `${(hourNumber % 12) || 12}:${minutes} ${amOrpm}`;
+    return new Date(startTime.substring(0,16)).toLocaleTimeString('en-US', {hour: 'numeric', minute: '2-digit'})
 }
